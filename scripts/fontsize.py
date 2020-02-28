82976ef8cce720dc4552ebd48f7ca3d44be7aabc
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-02-28 16:38:21 +0000

    Fix indentation in generated code from fontsize.py
    fontsize.py now correctly indents generated lines of code by using
    the indentation level of the next non-blank line.

--- scripts/fontsize.py
+++ scripts/fontsize.py
@@ -16,13 +16,15 @@ Note: On macOS/Linux you may need to use `python3` instead of `python`.
 import os
 from os import path
 from pathlib import Path
-import sys 
+import sys
 from typing import List
 
 assert sys.version_info >= (3, 6), "Please run with Python 3.6 or above."
 
 args = sys.argv[1:]
-assert args, "Font size must be supplied as an argument. For example, 'python fontsize.py 1' for a font size of 12."
+assert (
+    args
+), "Font size must be supplied as an argument. For example, 'python fontsize.py 1' for a font size of 12."
 
 # Set working directory to MODA folder.
 here = path.abspath(path.dirname(__file__))
@@ -31,30 +33,42 @@ os.chdir(Path(here).parent)
 gfs = "globalfontsize"
 comment = "Do not edit this line manually. See scripts/fontsize.py."
 
+
 def readlines(filename: str) -> List[str]:
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         lines = f.readlines()
 
     return lines
 
 
-def mutate_lines(filename:str, lines: List[str]) -> List[str]:
+def mutate_lines(filename: str, lines: List[str]) -> List[str]:
     lines = lines.copy()
     for index, item in enumerate(lines.copy()):
-        if item.rstrip().startswith(gfs):
-            new = f"{gfs} = {fontsize}; % {comment}\n"
+        if item.strip().startswith(gfs):
+            indent = get_indentation(lines[index + 1 :])
+            new = f"{indent}{gfs} = {fontsize}; % {comment}\n"
+
             lines[index] = new
             print(f"{filename}: editing line {index+1}.")
 
     return lines
 
+
+def get_indentation(lines: List[str]) -> str:
+    for l in lines:
+        if l.strip():
+            return " " * (len(l) - len(l.lstrip()))
+    return ""
+
+
 def write_lines(filename: str, lines: List[str]) -> None:
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.writelines(lines)
+
 
 if __name__ == "__main__":
     fontsize = int(args[0])
-    
+
     for filepath in Path(".").rglob("*.m"):
         filename = filepath.absolute()
 
